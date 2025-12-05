af2a44ec584e499723f1f657006a2bd81357f229
SAMU DOGAM <samudeogam62@gmail.com>
2025-12-05 17:47:10 +0530

    process.exit added but not working

--- Lecture-3 Create a Server/app.js
+++ Lecture-3 Create a Server/app.js
@@ -1,10 +1,11 @@
-const http = require ('http');
+const http = require("http");
 
-const server = http.createServer ((req,res) => {
+const server = http.createServer((req, res) => {
   console.log(req);
+  // process.exit();
 });
 
 const PORT = 3002;
-server.listen (PORT, () => {
-  console.log (`Server iss running http://localhost${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server iss running http://localhost${PORT}`);
 });
