b6afa1b96393b9e3a2366ed0da00c580c190c7d8
SAMU DOGAM <samudeogam62@gmail.com>
2025-12-06 10:14:49 +0530

    understanding set routing request res.end()

--- Lecture-3 Create a Server/app.js
+++ Lecture-3 Create a Server/app.js
@@ -2,12 +2,31 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-  res.write("</html>");
-  res.end();
+
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+    res.write("</html>");
+    res.end();
+  } else if (req.url === "/product") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Hello from my Product Section</h1></body>");
+    res.write("</html>");
+    res.end();
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>The End</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+    
+  
 });
 
 const PORT = 3002;
