5a2463bd2fc99ade42bd6b4a04d80e2a21d5fe13
SAMU DOGAM <samudeogam62@gmail.com>
2025-12-06 09:47:42 +0530

    response of server to the client

--- Lecture-3 Create a Server/app.js
+++ Lecture-3 Create a Server/app.js
@@ -1,11 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  // process.exit();
+  console.log(req.url, req.method, req.headers);
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 const PORT = 3002;
 server.listen(PORT, () => {
-  console.log(`Server iss running http://localhost${PORT}`);
+  console.log(`Server iss running http://localhost:${PORT}`);
 });
