cd588eef918d580dd0af8772aff50eed84d31dd5
SAMU DOGAM <samudeogam62@gmail.com>
2025-12-06 11:30:25 +0530

    created a form in user .js

--- Lecture-3 Create a Server/user.js
+++ Lecture-3 Create a Server/user.js
@@ -7,7 +7,20 @@ const server = http.createServer((req, res) => {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<head><title>My First Page</title></head>");
-    res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+    res.write("<body><h1>Enter your Details</h1>");
+    res.write("<form action='/submit-details' method='POST'>");
+    res.write(
+      "<input type+'text' name='username' placeholder='Enter your name'><br>"
+    );
+    res.write('<label for="male" >Male</label>');
+    res.write("<input type='radio'id='male' name='gender' value='male'/>");
+    res.write('<label for="female" >Female</label>');
+    res.write(
+      "<input type='radio' id='female' name='gender' value='female'/><br>"
+    );
+    res.write("<input type='submit' value- 'submit'/>");
+    res.write("</form>");
+    res.write("</body>");
     res.write("</html>");
     res.end();
   } else if (req.url === "/product") {
@@ -25,8 +38,6 @@ const server = http.createServer((req, res) => {
     res.write("</html>");
     res.end();
   }
-    
-  
 });
 
 const PORT = 3002;
